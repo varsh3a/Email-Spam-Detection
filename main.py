aac21c83fad4b03b18b3e9266b1427e4fe7ee089
Sri Varsha Yeshala <105740304+varsh3a@users.noreply.github.com>
2023-07-22 00:33:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+from win32com.client import Dispatch
+
+ps = PorterStemmer()
+
+def speak(text):
+    speak=Dispatch(("SAPI.SpVoice"))
+    speak.speak(text)
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title("Email Spam Detection")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+        speak("This is spam mail")
+    else:
+        st.header("Not Spam")
+        speak("This is not a spam mail")
